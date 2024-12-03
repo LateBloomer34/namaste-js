2ba0b69139cb783b8c135b68bb239c08402d56ae
LateBloomer34 <himnariyal30@gmail.com>
2024-12-03 10:11:51 +0530

    Undefined vs Not defined

---
+++ EP6 Undefined vs Not Defined Dec3/index.js
@@ -0,0 +1,34 @@
+// undefiend - not defined now, may be defined in future.
+//undefined is a primitive data type in JS.
+
+/* when a program runs a global execution context is created. 
+there are 2 main part of GEC , memory allocation phase and code execution phase . 
+in memory allocaction phase, memory allocated to all varibale and function, the value of the all varibales of the programe fill with a special keyword undefined,
+
+in code execution phase , respection value given to each varibale. 
+.
+
+
+Not defined-  when we try to access value of varibale which is not defined in programe i.e. not allocate memory to the variable.
+ex - */ 
+console.log(a)
+let h = 6;
+
+// o p - a is not defined.
+
+
+
+
+/* imprtant operation with undefined - 
+type of undefined is undefined;
+undefined===undefined  => true;
+
+1+undefined = NaN
+1-undefined = NaN
+1/undefined = NaN
+1*undefined = NaN
+
+"1"+undefined = 1undefiend
+true+undefined = NaN
+NaN+undefined = NaN
+*/
